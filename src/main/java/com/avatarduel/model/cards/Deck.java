975252b353d5f526da775bd0b5ecf1c2936dbad9
fabianusharry <fabianusharry@yahoo.com>
2020-04-19 15:16:42 +0700

    add documentation in model/cards

--- src/main/java/com/avatarduel/model/cards/Deck.java
+++ src/main/java/com/avatarduel/model/cards/Deck.java
@@ -8,9 +8,22 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.*;
 
+/**
+ * 
+ * Deck.java
+ * A Class that manage the Deck that a player have and use to draw
+ * ATTRIBUTES :
+ * Stack of Card : The Stack of Random Cards the player use to draw to hand in the game
+ * 
+ */
 public class Deck {
     private Stack<Card> cards;
 
+    /**
+     * Creates a new Deck Object
+     * @throws IOException  Exception when Creating the deck with other class constructors
+     * @throws URISyntaxException   exception when the URI syntax is incorrect
+     */
     public Deck() throws IOException, URISyntaxException {
         cards = new Stack<Card>();
         int nDeck = new Random().nextInt(21) + 40; //random nDeck 40-60
@@ -27,10 +40,18 @@ public class Deck {
         Collections.shuffle(cards);
     }
 
+    /**
+     * Return the Size of the Current Deck
+     * @return Current Deck Size
+     */
     public int size() {
         return cards.size();
     }
 
+    /**
+     * Take a card from the deck
+     * @return The Top of the Stack (Card drawn from the deck)
+     */
     public Card take() {
         Card takenCard = null;
         if (cards.size() > 0) {
@@ -39,6 +60,11 @@ public class Deck {
         return takenCard;
     }
 
+    /**
+     * Take bunch of cards from the deck
+     * @param nCard number of cards taken
+     * @return List of cards taken
+     */
     public List<Card> takes(int nCard) {
         List<Card> takenCards = new LinkedList<>();
         for (int i = 0; i < nCard; i++) {
--- src/main/java/com/avatarduel/model/cards/HandCards.java
+++ src/main/java/com/avatarduel/model/cards/HandCards.java
@@ -5,22 +5,51 @@ import com.avatarduel.model.card.Card;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 
+ * HandCards.java
+ * A Class that handles Cards that is currently in the hand of a player
+ * ATTRIBUTES :
+ * List of Card : Cards in the Hand of a player
+ */
 public class HandCards {
     private List<Card> cards;
 
+    /**
+     * Create a new HandCards object that consists of List of Card c
+     * @param c List of card
+     */
     public HandCards(List<Card> c) {
         cards = new LinkedList<Card>(c);
     }
 
+    /**
+     * Return the Size of the Current HandCards cards list
+     * @return Current cards Size
+     */
     public int size() {
         return cards.size();
     }
+
+    /**
+     * Get the Hand Cards of the player
+     * @return List of Player's Hand Cards
+     */
     public List<Card> getCards() { return cards; }
 
+    /**
+     * Add a card to the list of HandCard (Draw Phase)
+     * @param newCard card to be added
+     */
     public void add(Card newCard) {
         cards.add(newCard);
     }
 
+    /**
+     * Take a specified cards and remove it from the HandCards List
+     * @param index specified card index of list
+     * @return Specified card
+     */
     public Card take(int index) {
         Card takenCard = null;
         if (cards.size() > 0 && index < size()) {
@@ -29,6 +58,11 @@ public class HandCards {
         return takenCard;
     }
 
+    /**
+     * Take a specified cards from the HandCards List
+     * @param index specified card index in HandCards list
+     * @return Specified card
+     */
     public Card peek(int index) {
         Card takenCard = null;
         if (index >= 0 && index < size())
